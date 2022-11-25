4ba4ac0bec8416d8d288c5d517c524564111dc36
hoffmanaiden <hoffman.aiden@gmail.com>
2022-11-25 11:25:49 -0500

    stuggling to add a line

--- src/Tools.js
+++ src/Tools.js
@@ -3,6 +3,7 @@ import { useRef, useState, useEffect, useContext } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
 import { Physics, RigidBody, Debug, BallCollider, Attractor } from "@react-three/rapier";
 import { AppContext } from "./App";
+import { LineBasicMaterial } from "three";
 
 export function Launcher(props, { vec = new THREE.Vector3() }) {
   const { state, dispatch } = useContext(AppContext)
@@ -13,13 +14,25 @@ export function Launcher(props, { vec = new THREE.Vector3() }) {
     dispatch({type: 'clearDragObj'})
   }
   const ref = useRef()
+
+
+  const points = []
+  points.push(new THREE.Vector3(0,2,0))
+  points.push(new THREE.Vector3(0,2,0))
+
   useFrame(({ mouse, viewport }) => {
     vec.lerp({ x: (mouse.x * viewport.width) / 2, y: ((mouse.y * viewport.height) / 2) - 2, z: 0 }, 0.2)
     if (state.activeDragObj === 'Launcher') {
       ref.current.setTranslation(vec)
       console.log(vec)
+      points[1] = vec
     }
   })
+
+
+  const lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
+
+
   return (
     <group>
       {/* external ball */}
@@ -27,10 +40,13 @@ export function Launcher(props, { vec = new THREE.Vector3() }) {
         <sphereGeometry args={[2, 8, 8]} />
         <meshStandardMaterial color={'#32a852'} transparent={true} opacity={0.3} />
       </mesh>
+      {/* <line geometry={lineGeometry}>
+        <LineBasicMaterial attach="material" color={'#9c88ff'} linewidth={10} linecap={'round'} linejoin={'round'}/>
+      </line> */}
       {/* internal ball - handle */}
       <RigidBody colliders="ball" type={"kinematicPosition"} ref={ref} >
         <mesh 
-          position={state.handlePos}
+          position={[0,2,0]}
           onPointerDown={(event) => grab()}
           onPointerUp={(event) => letGo()}
         >
